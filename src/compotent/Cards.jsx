f11538f466e4023378cbd96e4ddedb42e4eda831
emmanuelle giffard <emmanuelle.giffard@gmail.com>
2024-02-28 23:00:15 +0100

    set game play

--- src/compotent/Cards.jsx
+++ src/compotent/Cards.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useState, useRef } from 'react'
 import Card from './Card'
 
 export default function Cards() {
@@ -20,12 +20,53 @@ export default function Cards() {
     { id: 6, name: 'nebula', status: '', img: '/images/07.jpg'},
     { id: 7, name: 'red green galaxy', status: '', img: '/images/08.jpg'},
     { id: 7, name: 'red green galaxy', status: '', img: '/images/08.jpg'},
-  ])
+  ].sort(() => Math.random() - .5))
+  const [previousCardState, setPreviousCardState] = useState(-1)
+  const previousIndex =useRef(-1)
+
+  const matchCheck = (currentCard) => {
+    if(cards[currentCard].id === cards[previousCardState].id){
+      cards[previousCardState].status = 'active matched'
+      cards[currentCard].status = 'active matched'
+      setPreviousCardState(-1)
+    }else{
+      cards[currentCard].status = 'active'
+      setCards([...cards])
+      setTimeout(() => {
+        setPreviousCardState(-1)
+        cards[currentCard].status = 'unmatch'
+        cards[previousCardState].status = 'unmatch'
+        setCards([...cards])
+      }, 1000);
+    }
+  }
+
+  const clickhandler = (index) => {
+    if(index !== previousIndex.current){
+      if(cards[index].status === 'active matched'){
+        alert('Doublon déjà trouvé')
+      }else{
+        if(previousCardState === -1){
+          previousIndex.current = index
+          cards[index].status = 'active'
+          setCards([...cards])
+          setPreviousCardState(index)
+        }else{
+          matchCheck(index)
+          previousIndex.current = -1
+        }
+      }
+
+    }else{
+      alert("Carte déjà selectionnée")
+    }
+
+  }
 
   return (
     <div className="container">
       {cards.map((card, index) => {
-        return <Card card={card} />
+        return <Card card={card} key={index} index={index}  clickhandler={clickhandler}/>
       })}
 
     </div>
